63b4f2e83bbba6305faa26ec937e73fd1f453a0d
Joe Kim <josephkim0224@gmail.com>
2017-07-31 11:41:00 -0400

    integrate nightwatch with a test

---
+++ tests/auth-flow.js
@@ -0,0 +1,15 @@
+module.exports = {
+
+  'get to signup page': (client) => {
+    client
+      .url(client.launchUrl)
+      .waitForElementVisible('.navbar', 1000)
+      .click('a[href]="/signup"')
+
+    client.assert.urlContains('signup')
+  },
+
+  'signup user and redirect to cart': (clinet) => {
+
+  }
+}
