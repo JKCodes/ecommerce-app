06421881ae64e7c43b19f89cd1a3e22ac44d1cb7
Joe Kim <josephkim0224@gmail.com>
2017-07-31 17:58:05 -0400

    add react router dom with passing signup link

--- src/App.js
+++ src/App.js
@@ -1,13 +1,62 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  NavLink
+} from 'react-router-dom'
+import { StyleSheet, css } from 'aphrodite';
+
+const styles = StyleSheet.create({
+  navbar: {
+    width: '100%',
+    background: '#18121e',
+    padding: '6px'
+  },
+
+  navlink: {
+    textDecoration: 'none',
+    padding: '6px',
+    color: 'white',
+    ':hover': {
+      background: '#fff',
+      color: '#18121e'
+    }
+  }
+})
+
+const Home = () => <div>Home</div>
+const Signup = () => <form className="signup_form"></form>
+const NotFound = () => <div>NotFound</div>
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="navbar">
-          <a href="/signup">Signup</a>
+      <Router>
+        <div className="App">
+          <div className="navbar">
+            <div className={css(styles.navbar)}>
+              <NavLink 
+                className={css(styles.navlink)}
+                to="/">
+                Home
+              </NavLink>
+              <NavLink 
+                className={css(styles.navlink)}
+                to="/signup">
+                Signup
+              </NavLink>
+            </div>
+          </div>
+          <hr />
+
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/signup" component={Signup} />
+            <Route component={NotFound} />
+          </Switch>
         </div>
-      </div>
+      </Router>
     );
   }
 }
