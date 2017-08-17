bef7f620d718ed475449b306468c7fc9320e5582
Joe Kim <josephkim0224@gmail.com>
2017-08-17 12:32:26 -0400

    connect and mapStateToProps for signupForm

--- src/containers/SignupForm.js
+++ src/containers/SignupForm.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class SignupForm extends Component {
 
   render() {
     return (
       <form>
-
+      
       </form>
     )
   }
 }
 
-export default SignupForm;
+const mapStateToProps = (state) => {
+  return {
+    signupFormData: state.signupFormData
+  }
+}
+
+export default connect(mapStateToProps)(SignupForm);
