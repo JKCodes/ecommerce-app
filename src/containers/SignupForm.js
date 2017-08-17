3e897fc63ab844f6a01b2a689ee8f2e33d291a29
Joe Kim <josephkim0224@gmail.com>
2017-08-17 12:37:02 -0400

    Finish layouting signup form

--- src/containers/SignupForm.js
+++ src/containers/SignupForm.js
@@ -4,9 +4,35 @@ import { connect } from 'react-redux';
 class SignupForm extends Component {
 
   render() {
+    const { email, password, password_confirmation } = this.props.signupFormData
     return (
       <form>
-      
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            name="email"
+            value={email}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            name="password"
+            value={password}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="password_confirmation">Password Confirmation:</label>
+          <input
+            type="password"
+            name="password_confirmation"
+            value={password_confirmation}
+          />
+        </div>
       </form>
     )
   }
