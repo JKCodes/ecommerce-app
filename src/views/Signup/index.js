3e985db14d1d35b28276bb3084997bdb00c4fd07
Joe Kim <josephkim0224@gmail.com>
2017-08-17 12:52:25 -0400

    remove unnessary imports

--- src/views/Signup/index.js
+++ src/views/Signup/index.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
 
 import SignupForm from '../../containers/SignupForm';
-import { signup } from '../../redux/modules/Auth/actions';
 
 class Signup extends Component {
 
