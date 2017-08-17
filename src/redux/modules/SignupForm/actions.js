494eb09bfafae4ac79ee8ca1977eccc253a1d2fc
Joe Kim <josephkim0224@gmail.com>
2017-08-17 13:25:50 -0400

    add call to rails api for signup actions

--- src/redux/modules/SignupForm/actions.js
+++ src/redux/modules/SignupForm/actions.js
@@ -1,6 +1,26 @@
+const API_URL = process.env.REACT_APP_API_URL;
+
 export const updateSignupFormData = (signupFormData) => {
   return {
     type: 'UPDATED_FORM_DATA',
     signupFormData
+  }
+}
+
+export const createUser = (userData) => {
+  console.log(userData)
+  return dispatch => {
+    return fetch(`${API_URL}/users`, {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ user: userData })
+    })
+    .then(response => response.json())
+    .then(user => {
+        debugger
+    })
+    .catch(error => console.log(error))
   }
 }
