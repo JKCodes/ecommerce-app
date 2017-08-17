7a9d02d6876b97016be1f7dd2c924696a5f5209b
Joe Kim <josephkim0224@gmail.com>
2017-08-17 18:23:18 -0400

    removed Auth related code from SignupForm and changed wording of router to history

--- src/redux/modules/Auth/actions.js
+++ src/redux/modules/Auth/actions.js
@@ -1,6 +1,8 @@
 import 'isomorphic-fetch';
 import { reset, SubmissionError } from 'redux-form'
 
+const API_URL = process.env.REACT_APP_API_URL;
+
 export const authenticationRequest = () => {
   return {
     type: 'AUTHENTICATION_REQUEST'
@@ -14,10 +16,10 @@ export const setCurrentUser = (user) => {
   }
 }
 
-export const signup = (userDetails, router) => {
+export const signup = (userDetails, history) => {
   return dispatch => {
     dispatch(authenticationRequest())
-    return fetch('/users', {
+    return fetch(`${API_URL}/users`, {
       method: 'POST',
       headers: {
         'Accept': 'application/json',
@@ -27,11 +29,15 @@ export const signup = (userDetails, router) => {
     })
     .then(response => response.json())
     .then(body => {
-      const slug = body.user.email.split('@')[0];
-      localStorage.setItem('e.shop.token', body.token);
-      dispatch(setCurrentUser(body.user));
-      dispatch(reset('signup'));
-      router.history.replace(`/users/${slug}/profile`);
+      if (!body.errors) {
+        const slug = body.user.email.split('@')[0];
+        localStorage.setItem('e.shop.token', body.token);
+        dispatch(setCurrentUser(body.user));
+        dispatch(reset('signup'));
+        history.replace(`/users/${slug}/profile`);        
+      } else {
+          Object.keys(body.errors).forEach((key) => console.log(`${key} ${body.errors[key]}`))
+      }
     })
     .catch(err => {
       throw new SubmissionError(err)
--- src/redux/modules/SignupForm/actions.js
+++ src/redux/modules/SignupForm/actions.js
@@ -1,36 +1,6 @@
-const API_URL = process.env.REACT_APP_API_URL;
-
 export const updateSignupFormData = (signupFormData) => {
   return {
     type: 'UPDATED_FORM_DATA',
     signupFormData
-  }
-}
-
-export const setUser = (user) => {
-  return {
-    type: 'AUTHENTICATION_SUCCESS',
-    user
-  }
-}
-
-export const createUser = (userData) => {
-  return dispatch => {
-    return fetch(`${API_URL}/users`, {
-      method: "POST",
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ user: userData })
-    })
-    .then(response => response.json())
-    .then(user => {
-        if (!user.errors) {
-          dispatch(setUser(user))
-        } else {
-          Object.keys(user.errors).forEach((key) => console.log(`${key} ${user.errors[key]}`))
-        }
-    })
-    .catch(error => console.log(error))
   }
 }
