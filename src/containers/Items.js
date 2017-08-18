4c10ccce6967db469edae74b3742b1c849d6b237
Joe Kim <josephkim0224@gmail.com>
2017-08-18 11:46:28 -0400

    add Add a new Item link on Items container

--- src/containers/Items.js
+++ src/containers/Items.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { getAllItems } from '../redux/modules/Items/actions'
 import ItemCard from '../components/ItemCard'
 
@@ -16,7 +17,8 @@ class Items extends Component {
   render() {
     return (
       <div>
-        {this.props.items.map(item => <ItemCard key={item.id} onClick={this.handleOnClick} item={item} />)}
+        <Link to={`/items/new`}>Add a new Item</Link>
+        {this.props.items.map(item => <Link key={item.id} to={`/items/${item.id}`}><ItemCard item={item} /></Link>)}
       </div>
     )
   }
