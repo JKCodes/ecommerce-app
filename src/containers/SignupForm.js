8a1917329d85e66a52af31ecebab5bb060b84eec
Joe Kim <josephkim0224@gmail.com>
2017-08-18 16:50:46 -0400

    add styles to Signup form

--- src/containers/SignupForm.js
+++ src/containers/SignupForm.js
@@ -3,6 +3,44 @@ import { connect } from 'react-redux';
 import { updateSignupFormData } from '../redux/modules/SignupForm/actions'
 import { signup } from '../redux/modules/Auth/actions'
 import { withRouter } from 'react-router-dom';
+import { StyleSheet, css } from 'aphrodite';
+
+const styles = StyleSheet.create({
+  form: {
+    margin: '0 auto',
+    width: '205px'
+  },
+
+  label: {
+    display: 'block',
+    paddingLeft: '10px',
+    margin: '15px auto 5px',
+  },
+
+  input: {
+    width: '200px',
+    height: '20px',
+    borderRadius: '5px'
+  },
+
+  button: {
+    width: '200px',
+    fontSize: '1.3em',
+    textAlign: 'center',
+    margin: '30px auto',
+    paddingTop: '10px',
+    paddingBottom: '10px',
+    border: '2px solid blue',
+    borderRadius: '15px',
+    background: 'blue',
+    color: "#fff",
+    boxShadow: '2px 2px 2px 2px #ccc',
+    ':hover': {
+      background: '#fff',
+      color: "blue"
+    }
+  }
+})
 
 class SignupForm extends Component {
 
@@ -22,10 +60,11 @@ class SignupForm extends Component {
   render() {
     const { email, password, password_confirmation } = this.props.signupFormData
     return (
-      <form onSubmit={this.handleOnSubmit}>
+      <form className={css(styles.form)} onSubmit={this.handleOnSubmit}>
         <div>
-          <label htmlFor="email">Email:</label>
+          <label className={css(styles.label)} htmlFor="email">Email:</label>
           <input
+            className={css(styles.input)}
             type="email"
             onChange={this.handleOnChange}
             name="email"
@@ -34,8 +73,9 @@ class SignupForm extends Component {
         </div>
 
         <div>
-          <label htmlFor="password">Password:</label>
+          <label className={css(styles.label)} htmlFor="password">Password:</label>
           <input
+            className={css(styles.input)}
             type="password"
             onChange={this.handleOnChange}
             name="password"
@@ -44,8 +84,9 @@ class SignupForm extends Component {
         </div>
 
         <div>
-          <label htmlFor="password_confirmation">Password Confirmation:</label>
+          <label className={css(styles.label)} htmlFor="password_confirmation">Password Confirmation:</label>
           <input
+            className={css(styles.input)}
             type="password"
             onChange={this.handleOnChange}
             name="password_confirmation"
@@ -53,7 +94,7 @@ class SignupForm extends Component {
           />
         </div>
 
-        <button type="submit">Create New Account</button>
+        <button className={css(styles.button)} type="submit">Create New Account</button>
       </form>
     )
   }
