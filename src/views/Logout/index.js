93d895339017e9737d0cb62b7a540a97fff748d6
Joe Kim <josephkim0224@gmail.com>
2017-08-18 15:06:54 -0400

    add styling to logout view

--- src/views/Logout/index.js
+++ src/views/Logout/index.js
@@ -2,12 +2,22 @@ import React, { Component } from 'react';
 
 import LogoutForm from '../../containers/LogoutForm';
 
+import { StyleSheet, css } from 'aphrodite';
+
+const styles = StyleSheet.create({
+  h3: {
+    width: '65%',
+    margin: '0 auto 20px',
+    textAlign: 'center'
+  }
+})
+
 class Logout extends Component {
 
   render() {
     return (
       <div className="logout">
-        <h3>Are you sure you want to logout?</h3>
+        <h3 className={css(styles.h3)}>Are you sure you want to logout?</h3>
         <LogoutForm />
       </div>
     )
