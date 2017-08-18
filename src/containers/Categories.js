2d3f1b83900c7d887abc4b428f7db834c2ec73b1
Joe Kim <josephkim0224@gmail.com>
2017-08-18 09:12:42 -0400

    add basic render function on Categories container

---
+++ src/containers/Categories.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Categories extends Component {
+
+  render() {
+    return (
+      <div>
+        Some info about Categories
+      </div>
+    )
+  }
+}
+
+export default Categories;
