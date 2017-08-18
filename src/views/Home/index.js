31b5792121ff7fbfacf8789c3ce4e0cb8a6d57b1
Joe Kim <josephkim0224@gmail.com>
2017-08-18 15:12:25 -0400

    add stylilng to Home page

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,5 +1,26 @@
 import React from 'react';
 
-const Home = () => <div><h3>Home</h3></div>
+import { StyleSheet, css } from 'aphrodite';
 
+const styles = StyleSheet.create({
+  center: {
+    margin: '0 auto',
+    textAlign: 'center'
+  },
+  adjustH2: {
+    marginTop: '10px',
+    marginBottom: '0',
+    fontSize: '1.1em'
+  }
+})
+
+const Home = () => (
+  <div>
+    <h1 className={css(styles.center)}>Welcome to Ecommerce App!</h1>
+    <h2 className={css(styles.adjustH2)}>Feel Free to browse through the Items</h2>
+    <h2 className={css(styles.adjustH2)}>You do not have to be logged in to view the items</h2>
+    <h2 className={css(styles.adjustH2)}>You must, however, be logged in to access your cart and to add items to your cart</h2>
+    <h2 className={css(styles.adjustH2)}>So, click on any of the Navigational links above to get started!</h2>
+  </div>
+)
 export default Home;
