07266420c77da78917c115096a41591fe2207466
Joe Kim <josephkim0224@gmail.com>
2017-08-18 15:19:32 -0400

    add styling to Category view

--- src/views/Category/index.js
+++ src/views/Category/index.js
@@ -2,12 +2,22 @@ import React, { Component } from 'react';
 
 import Categories from '../../containers/Categories';
 
+import { StyleSheet, css } from 'aphrodite';
+
+const styles = StyleSheet.create({
+  h1: {
+    textAlign: 'center',
+    marginTop: '5px',
+    marginBottom: '5px'
+  }
+})
+
 class Category extends Component {
 
   render() {
     return (
       <div className="category">
-        <h3>Categories</h3>
+        <h1 className={css(styles.h1)}>Categories</h1>
         <Categories />
       </div>
     )
