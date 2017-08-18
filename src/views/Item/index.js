ffe8946299904c5d680080d2aa02a6fdd8b0ec7f
Joe Kim <josephkim0224@gmail.com>
2017-08-18 15:33:46 -0400

    styles the Items heading on Items page

--- src/views/Item/index.js
+++ src/views/Item/index.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
 
 import Items from '../../containers/Items';
+import { StyleSheet, css } from 'aphrodite';
+
+const styles = StyleSheet.create({
+  h1: {
+    textAlign: 'center',
+    marginTop: '5px',
+    marginBottom: '5px'
+  }
+})
 
 class Item extends Component {
 
   render() {
     return (
       <div className="item">
-        <h3>Items</h3>
+        <h1 className={css(styles.h1)}>Items</h1>
         <Items />
       </div>
     )
