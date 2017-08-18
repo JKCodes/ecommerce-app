a87c363d899902072645362046c8695fd20fc0d3
Joe Kim <josephkim0224@gmail.com>
2017-08-18 16:11:26 -0400

    added styles for item show page

--- src/containers/ItemShow.js
+++ src/containers/ItemShow.js
@@ -1,12 +1,49 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+import { StyleSheet, css } from 'aphrodite';
+
+const styles = StyleSheet.create({
+  h1: {
+    textAlign: 'center'
+  },
+
+  p: {
+    textAlign: 'center',
+    fontSize: '1.2em',
+    marginBottom: '2px',
+    marginTop: '2px'
+  },
+
+  button: {
+    display: 'block',
+    textDecoration: 'none',
+    width: '150px',
+    fontSize: '1.3em',
+    color: 'black',
+    textAlign: 'center',
+    margin: '0 auto',
+    paddingTop: '5px',
+    paddingBottom: '5px',
+    border: '2px solid #444',
+    borderRadius: '15px',
+    background: 'magenta',
+    color: "#555",
+    marginTop: "20px",
+    boxShadow: '2px 2px 2px 2px #ccc',
+    ':hover': {
+      background: '#444',
+      color: "magenta"
+    }
+  }
+})
+
 const ItemShow = ({ item }) => (
   <div>
-    <h2>{item.title}</h2>
-    <p>Price: ${item.price}</p>
-    <p>Inventory: {item.inventory}</p>
-    <button>Add to Cart - future plans</button>
+    <h1 className={css(styles.h1)}>{item.title}</h1>
+    <p className={css(styles.p)}>Price: ${item.price}</p>
+    <p className={css(styles.p)}>Inventory: {item.inventory}</p>
+    <button className={css(styles.button)}>Add to Cart</button>
   </div>
 )
 
