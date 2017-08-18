34b5897acf88502a28fcbb73bdcead9527aa724f
Joe Kim <josephkim0224@gmail.com>
2017-08-18 11:48:45 -0400

    add actions to Items actions

---
+++ src/redux/modules/Items/actions.js
@@ -0,0 +1,26 @@
+import 'isomorphic-fetch';
+import { reset } from 'redux-form'
+
+const API_URL = process.env.REACT_APP_API_URL;
+
+const setItems = (items) => {
+  return {
+    type: 'GET_ITEMS_SUCCESS',
+    items
+  }
+}
+
+export const addItem = () => {
+  return {
+    type: 'GEEE'
+  }
+}
+
+export const getAllItems = () => {
+  return dispatch => {
+    return fetch(`${API_URL}/items`)
+    .then(response => response.json())
+    .then(items => dispatch(setItems(items)))
+    .catch(error => console.log(error))
+  }
+}
