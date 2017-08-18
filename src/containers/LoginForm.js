c8d60eeef21f51422b95b03016395fe5da8039bd
Joe Kim <josephkim0224@gmail.com>
2017-08-18 16:54:23 -0400

    add styles to login form

--- src/containers/LoginForm.js
+++ src/containers/LoginForm.js
@@ -3,6 +3,44 @@ import { connect } from 'react-redux';
 import { updateLoginFormData } from '../redux/modules/LoginForm/actions';
 import { login } from '../redux/modules/Auth/actions'
 import { withRouter } from 'react-router-dom';
+import { StyleSheet, css } from 'aphrodite';
+
+const styles = StyleSheet.create({
+  form: {
+    margin: '0 auto',
+    width: '205px'
+  },
+
+  label: {
+    display: 'block',
+    paddingLeft: '10px',
+    margin: '15px auto 5px',
+  },
+
+  input: {
+    width: '200px',
+    height: '20px',
+    borderRadius: '5px'
+  },
+
+  button: {
+    width: '200px',
+    fontSize: '1.3em',
+    textAlign: 'center',
+    margin: '30px auto',
+    paddingTop: '10px',
+    paddingBottom: '10px',
+    border: '2px solid blue',
+    borderRadius: '15px',
+    background: 'blue',
+    color: "#fff",
+    boxShadow: '2px 2px 2px 2px #ccc',
+    ':hover': {
+      background: '#fff',
+      color: "blue"
+    }
+  }
+})
 
 class LoginForm extends Component {
 
@@ -23,10 +61,11 @@ class LoginForm extends Component {
     const { email, password } = this.props.loginFormData
 
     return (
-      <form onSubmit={this.handleOnSubmit}>
+      <form className={css(styles.form)} onSubmit={this.handleOnSubmit}>
         <div>
-          <label htmlFor="email">Email:</label>
+          <label htmlFor="email">Email</label>
           <input
+            className={css(styles.input)}
             type="email"
             onChange={this.handleOnChange}
             name="email"
@@ -35,8 +74,9 @@ class LoginForm extends Component {
         </div>
 
         <div>
-          <label htmlFor="password">Password:</label>
+          <label htmlFor="password">Password</label>
           <input
+            className={css(styles.input)}
             type="password"
             onChange={this.handleOnChange}
             name="password"
@@ -44,7 +84,7 @@ class LoginForm extends Component {
           />
         </div>
 
-        <button type="submit">Login</button>
+        <button className={css(styles.button)} type="submit">Login</button>
       </form>
     )
   }
