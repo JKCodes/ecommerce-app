88b5223279068f40fb91f3bf824f812eeefe53cd
Joe Kim <josephkim0224@gmail.com>
2017-08-18 15:53:17 -0400

    style add a new item button

--- src/containers/Items.js
+++ src/containers/Items.js
@@ -3,11 +3,36 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import ItemCard from '../components/ItemCard'
 
+import { StyleSheet, css } from 'aphrodite';
+
+const styles = StyleSheet.create({
+  link: {
+    display: 'block',
+    textDecoration: 'none',
+    width: '150px',
+    fontSize: '1.3em',
+    color: 'black',
+    textAlign: 'center',
+    margin: '0 auto',
+    paddingTop: '5px',
+    paddingBottom: '5px',
+    border: '2px solid #444',
+    borderRadius: '15px',
+    background: 'cyan',
+    color: "#555",
+    boxShadow: '2px 2px 2px 2px #ccc',
+    ':hover': {
+      background: '#444',
+      color: "cyan"
+    }
+  }
+})
+
 class Items extends Component {
   render() {
     return (
       <div>
-        <Link to={`/items/new`}>Add a new Item</Link>
+        <Link className={css(styles.link)} to={`/items/new`}>Add a new Item</Link>
         {this.props.items.map(item => <Link key={item.id} to={`/items/${item.id}`}><ItemCard item={item} /></Link>)}
       </div>
     )
