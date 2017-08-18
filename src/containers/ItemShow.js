69ac2f125217445b5a7914d57d0936308ad5bb0e
Joe Kim <josephkim0224@gmail.com>
2017-08-18 13:08:10 -0400

    ItemShow.js shows some details of the chosen item allong with a currently dummied out add to cart button

--- src/containers/ItemShow.js
+++ src/containers/ItemShow.js
@@ -1,18 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 
-class ItemShow extends Component {
-  render() {
-    return (
-      <div>
-        Hi
-      </div>
-    )
-  }
-}
+const ItemShow = ({ item }) => (
+  <div>
+    <h2>{item.title}</h2>
+    <p>Price: ${item.price}</p>
+    <p>Inventory: {item.inventory}</p>
+    <button>Add to Cart - future plans</button>
+  </div>
+)
+
 
 const mapStateToProps = (state, ownProps) => {
-  const item = state.items.find(item => item.id === ownProps.match.params.itemId)
+  console.log(state.items)
+  const item = state.items.find(item => item.id === +ownProps.match.params.itemId)
 
   if (item) {
     return { item }
