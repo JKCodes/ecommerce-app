957affae5b945ee1e93f0bce7e2968af87452296
Joe Kim <josephkim0224@gmail.com>
2017-08-18 09:24:00 -0400

    connect Categories container with redux store

--- src/containers/Categories.js
+++ src/containers/Categories.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import CategoryItem from '../components/CategoryItem'
+
 
 class Categories extends Component {
 
   render() {
     return (
       <div>
-        Some info about Categories
+        {this.props.categories.map(category => <CategoryItem key={category.id} category={category} />)}
       </div>
     )
   }
 }
 
-export default Categories;
+const mapStateToProps = (state) => {
+  return ({
+    categories: state.categories
+  })
+}
+
+export default connect(mapStateToProps)(Categories);
