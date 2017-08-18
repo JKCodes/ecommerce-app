2610470ee95362b4e0c6172116a248acd0bbdc4e
Joe Kim <josephkim0224@gmail.com>
2017-08-18 16:42:57 -0400

    add styles for Login heading

--- src/views/Login/index.js
+++ src/views/Login/index.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 
 import LoginForm from '../../containers/LoginForm';
+import { StyleSheet, css } from 'aphrodite';
+
+const styles = StyleSheet.create({
+  h1: {
+    textAlign: 'center'
+  }
+})
 
 class Login extends Component {
 
   render() {
     return (
       <div className="login">
-        <h3>Login below</h3>
+        <h1 className={css(styles.h1)}>Login below</h1>
         <LoginForm />
       </div>
     )
