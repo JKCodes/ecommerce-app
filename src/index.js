c7b5648b5dfdde0364a95f6b22ae485739649249
Joe Kim <josephkim0224@gmail.com>
2017-08-18 07:47:57 -0400

    removed unnecessary aphorodite import on main index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
-import { StyleSheet, css } from 'aphrodite';
 import store from './redux/store'
 
 ReactDOM.render(
