a6d4f82b686d5f43a4e6959c5f880a61bd09ceba
Joe Kim <josephkim0224@gmail.com>
2017-08-18 15:17:25 -0400

    add styling to NotFound view

--- src/views/NotFound/index.js
+++ src/views/NotFound/index.js
@@ -1,5 +1,19 @@
 import React from 'react';
 
-const NotFound = () => <div>NotFound</div>
+import { StyleSheet, css } from 'aphrodite';
 
+const styles = StyleSheet.create({
+  h3: {
+    textAlign: 'center',
+    marginTop: '5px',
+    marginBottom: '0'
+  }
+})
+
+const NotFound = () => (
+  <div>
+    <h3 className={css(styles.h3)}>The requested resource could not be found.</h3>
+    <h3 className={css(styles.h3)}>Please note that some resources required the user to be logged in first</h3>
+  </div>
+)
 export default NotFound;
