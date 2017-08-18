506a3329e9f4f4078fb32344c0e619bf64ac3ae9
Joe Kim <josephkim0224@gmail.com>
2017-08-18 15:31:48 -0400

    styled the category buttons

--- src/containers/Categories.js
+++ src/containers/Categories.js
@@ -1,6 +1,22 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import CategoryItem from '../components/CategoryItem'
+import { StyleSheet, css } from 'aphrodite';
+
+const styles = StyleSheet.create({
+  button: {
+    textAlign: 'center',
+    margin: '1%',
+    width: '31%',
+    height: '65px',
+    background: '#fff',
+    ':hover': {
+      background: 'lightGray'
+    },
+    boxShadow: '1px 1px 2px 2px #ccc',
+    borderRadius: '10%'
+  }
+})
 
 class Categories extends Component {
   handleOnClick = (e) => {
@@ -10,7 +26,7 @@ class Categories extends Component {
   render() {
     return (
       <div>
-        {this.props.categories.map(category => <button onClick={this.handleOnClick} key={category.id}><CategoryItem category={category} /></button>)}
+        {this.props.categories.map(category => <button className={css(styles.button)} onClick={this.handleOnClick} key={category.id}><CategoryItem category={category} /></button>)}
       </div>
     )
   }
