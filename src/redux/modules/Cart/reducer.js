7d4a908cdf5a38505ed8d451d0db3248d4003f0a
Joe Kim <josephkim0224@gmail.com>
2017-09-29 13:09:00 -0400

    updated Cart reducer to include GET_CART_SUCCESS

--- src/redux/modules/Cart/reducer.js
+++ src/redux/modules/Cart/reducer.js
@@ -1,1 +1,10 @@
 const initialState = []
+
+export default (state = initialState, action) => {
+  switch(action.type) {
+    case 'GET_CART_SUCCESS':
+      return action.cart;
+    default:
+      return state;
+  }
+}
