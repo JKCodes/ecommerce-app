ed003505052ae6eff113ae9beef6ede2a86773ba
Joe Kim <josephkim0224@gmail.com>
2017-10-06 14:13:59 -0400

    Fix inconsistent codes from the main index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import App from './containers/App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import App from './containers/App'
+import registerServiceWorker from './registerServiceWorker'
 import store from './redux/store'
 
 ReactDOM.render(
@@ -11,6 +11,6 @@ ReactDOM.render(
   </Provider>,
   
   document.getElementById('root')
-);
-registerServiceWorker();
+)
+registerServiceWorker()
  
