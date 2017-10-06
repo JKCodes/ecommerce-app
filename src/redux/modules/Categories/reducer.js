b3831a98e2c3523aea1139693f77431641e34813
Joe Kim <josephkim0224@gmail.com>
2017-10-06 14:09:38 -0400

    Fix inconsistent codes on Categories reducer

--- src/redux/modules/Categories/reducer.js
+++ src/redux/modules/Categories/reducer.js
@@ -3,8 +3,8 @@ const initialState = []
 export default (state = initialState, action) => {
   switch(action.type) {
     case 'GET_CATEGORIES_SUCCESS':
-      return action.categories;
+      return action.categories
     default:
-      return state;
+      return state
   }
 }
