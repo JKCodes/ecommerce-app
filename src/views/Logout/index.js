cd579350b2ec309ce0a53a913354722b719541ee
Joe Kim <josephkim0224@gmail.com>
2017-10-06 14:12:41 -0400

    Fix inconsistent codes from Logout View

--- src/views/Logout/index.js
+++ src/views/Logout/index.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
-import LogoutForm from '../../containers/LogoutForm';
+import LogoutForm from '../../containers/LogoutForm'
 
-import { StyleSheet, css } from 'aphrodite';
+import { StyleSheet, css } from 'aphrodite'
 
 const styles = StyleSheet.create({
   h3: {
