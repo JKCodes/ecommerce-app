1f32fcc0aa5f4de4c5aba8f24fe383ffb215efcc
Joe Kim <josephkim0224@gmail.com>
2017-10-06 14:10:33 -0400

    Remove unnecessary commented out code and fix inconsistent code on Items reducer

--- src/redux/modules/Items/reducer.js
+++ src/redux/modules/Items/reducer.js
@@ -3,11 +3,10 @@ const initialState = []
 export default (state = initialState, action) => {
   switch(action.type) {
     case 'GET_ITEMS_SUCCESS':
-      return action.items;
+      return action.items
     case 'ADD_NEW_ITEM_SUCCESS':
       return state.concat(action.item)
-      //return [...state, action.item]
     default:
-      return state;
+      return state
   }
 }
