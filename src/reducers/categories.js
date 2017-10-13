92543b21829c95a8744551f63a00acc5c8e84c7f
Joe Kim <josephkim0224@gmail.com>
2017-10-13 15:13:38 -0400

    added reducer for when a new category is successfully added to the backend database

--- src/reducers/categories.js
+++ src/reducers/categories.js
@@ -4,6 +4,12 @@ export default (state = initialState, action) => {
   switch(action.type) {
     case 'GET_CATEGORIES_SUCCESS':
       return action.categories
+    case 'ADD_NEW_CATEGORY_SUCCESS':
+      state.push(action.category)
+      state.sort(function(a, b) {
+        return a.title > b.title
+      })
+      return state
     default:
       return state
   }
