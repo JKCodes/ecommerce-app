06d6930b8388f4fe22b9615e93fb328f72b41b0a
Joe Kim <josephkim0224@gmail.com>
2018-01-09 14:42:20 -0500

    start updating main index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,17 @@ import { Provider } from 'react-redux'
 import App from './containers/App'
 import registerServiceWorker from './registerServiceWorker'
 import store from './store'
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  NavLink
+} from 'react-router-dom'
+import {
+  Category, CategoryNew, Home, Item, ItemNew, ItemShow,
+  Login, Logout, NotFound, Signup
+} from './layout'
+import { Header, Footer } from './components/containers'
 
 ReactDOM.render(
   <Provider store={store}>
