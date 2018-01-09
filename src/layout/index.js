a7ef90a64d1776a76f11ad721fac3b69cd64784d
Joe Kim <josephkim0224@gmail.com>
2018-01-09 14:37:29 -0500

    Move all layout pages to a new folder

---
+++ src/layout/index.js
@@ -0,0 +1,25 @@
+import Category from './Category'
+import CategoryNew from './CategoryNew'
+import Home from './Home'
+import Item from './Item'
+import ItemNew from './ItemNew'
+import ItemShow from './ItemShow'
+import Login from './Login'
+import Logout from './Logout'
+import NotFound from './NotFound'
+import Signup from './Signup'
+
+export {
+
+  Category,
+  CategoryNew,
+  Home,
+  Item,
+  ItemNew,
+  ItemShow,
+  Login,
+  Logout,
+  NotFound,
+  Signup
+
+}
