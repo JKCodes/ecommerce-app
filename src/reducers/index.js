b9706ebc50e410285f0331421bd218b670825970
Joe Kim <josephkim0224@gmail.com>
2018-01-09 14:37:53 -0500

    rename all reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,11 @@
+import authReducer from './authReducer'
+import categoryReducer from './categoryReducer'
+import itemReducer from './itemReducer'
+import cartReducer from './cartReducer'
+
+export {
+  authReducer,
+  categoryReducer,
+  authReducer,
+  cartReducer
+}
